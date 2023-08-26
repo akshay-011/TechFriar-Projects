3e0f20a9984cf4a9b45703eb3d7ad82a9382b3cc
akshay-011 <akshaykumarkannan2003826@gmail.com>
2023-08-26 15:01:38 +0530

    Designed 2 pages

--- RPG-Day-01/index.js
+++ RPG-Day-01/index.js
@@ -23,3 +23,20 @@ function rpg(key, length) {
   
     return password;
   }
+function validate(){
+    const email = document.getElementById('inputEmail4').value;
+    const name = document.getElementById('inputPassword4').value;
+    const address = document.getElementById('inputAddress').value;
+    const city = document.getElementById('inputCity').value;
+    const state = document.getElementById('inputState').value;
+    const zip = document.getElementById('inputZip').value;  
+    if (!email || !name || !address || !city || state === 'Choose' || !zip) {
+        document.getElementById("warning").innerHTML = `<div class="alert alert-warning alert-dismissible fade show" role="alert">
+        <strong>Warning !! <br></strong> You should check in on some of those fields below.
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>`
+    }
+
+}
